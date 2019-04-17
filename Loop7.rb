0eda7258d01d04effa04ad0d4d7d4ea7565771e1
Yoann <leyonodu72@gmail.com>
2019-04-17 16:09:56 +0200

    Update Loop7.rb

--- Loop7.rb
+++ Loop7.rb
@@ -1,9 +1,7 @@
 live_loop :loop7 do
-
   sync :bat
   with_fx :reverb, room: 0.8, mix: 0.8 do
-    use_synth :pretty_bell
-    play (ring :E4, :Fs4, :B4).choose ,sustain: 3, amp: 0.5, release: 0.8, cutoff: 120
-    sleep 12
+    sample :loop_industrial, beat_stretch: 2, amp: 0.3
+    sleep 24
   end
 end
